646919c9332dfef6224a6e21f5111c5d3787824d
mohan2508 <115917715+mohan2508@users.noreply.github.com>
2024-12-08 12:38:06 +0530

    Update register.py

--- register.py
+++ register.py
@@ -1,51 +1,46 @@
 import streamlit as st 
-import mysql.connector
+import sqlite3
 
-st.header(':orange[MohanClasses]')
-st.image('https://th.bing.com/th/id/OIP.rhl1XV7nIE3sJymInBApNQHaEK?w=500&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7')
+conn=sqlite3.connect('register.db',check_same_thread=False)
+cursor=conn.cursor()
 
-mydb=mysql.connector.connect(
-    host="localhost",
-    port='3306',
-    user="root",
-    #password="aparnaa12",
-    database="mohanclasses",
-    #auth_plugin='mysql_native_password'
-)
-mycursor=mydb.cursor()
-
-
-def main():
-    st.title(':rainbow[Please Fill This Form]')
-  
-    name = st.text_input('Enter your Name :')
-    email =st.text_input('Enter your Email-Id :')
-    phone =st.text_input('Enter your Phone :')
-    Class =st.text_input('Enter yor Standard :')
-    school=st.text_input('Enter your School Name :')
-    
-    if st.button(":green[Register]"):
-        sql="insert into register_form (name,email,phone,Class,school) values(%s,%s,%s,%s,%s)"
-        val=(name,email,phone,Class,school)
-        mycursor.execute(sql,val)
-        mydb.commit()
-        st.success('Registration has been completed successfully')
-        st.balloons()
-    
-    
-    
-       
+def formCreation():
+    st.write(':rainbow[Please Fill This Form]')
+    with st.form(key='Registration Form'):
+        name = st.text_input('Enter your Name :')
+        standard =st.text_input('Enter your Class :')
+        board =st.text_input('Enter CBSE/State :')
+        place =st.text_input('Enter yor Location :')
+        online=st.text_input('Enter time of your class :')
+        contact =st.text_input('Enter your Mobile:')
+        submit =st.form_submit_button(label='Register')
         
-              
+        if submit == True:
+            st.success('Your registration has been successful')
+            addInfo(name,standard,board,place,online,contact)       
     
     
-if __name__=="__main__":
-    main()
+def addInfo(a,b,c,x,y,z):
     
+    cursor.execute(
+         """
+    CREATE TABLE IF NOT EXISTS registration(Name Text(50),Standard Text(10),Board Text(10),
+    Place Text(25),Online_Time Text(10),Contact Text(10))
+    """
+    )
+    cursor.execute("INSERT INTO registration values (?,?,?,?,?,?)",(a,b,c,x,y,z))
+    conn.commit()
+    conn.close()
+    st.success('User has been added to the SQLITE database')
+    
+
             
     
-  
+formCreation()    
+    
     
            
     
+    
+
     
